69df10f1c0ede1f433713351d2d72c6b1a266a70
Jorge Luis Boeira Bavaresco <jlbavaresco@gmail.com>
2020-12-22 14:20:36 -0300

    Ajustes para verificar se registro foi atualizado, removido ou recuperado no caso de um id inexistente

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const { request, response } = require('express')
 const app = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended :true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
 const getProdutos = (request, response) => {
-    pool.query('SELECT * FROM produtos', (error, results) =>{
+    pool.query('SELECT * FROM produtos', (error, results) => {
         if (error) {
             throw error
         }
@@ -20,56 +20,55 @@ const getProdutos = (request, response) => {
 }
 
 const addProduto = (request, response) => {
-    const {nome, preco, estoque} = request.body
+    const { nome, preco, estoque } = request.body
 
     pool.query(
         'INSERT INTO PRODUTOS (nome, preco, estoque) values ($1, $2, $3)',
-        [nome, preco, estoque], 
+        [nome, preco, estoque],
         (error) => {
-            if (error){
+            if (error) {
                 throw error
             }
-            response.status(201).json({ status: 'success', message: 'Produto criado com sucesso'})
+            response.status(201).json({ status: 'success', message: 'Produto criado com sucesso' })
         }
     )
 }
 
 
 const updateProduto = (request, response) => {
-    const {codigo, nome, preco, estoque} = request.body
-
+    const { codigo, nome, preco, estoque } = request.body
     pool.query(
         'UPDATE produtos set nome = $1, preco=$2, estoque = $3 where codigo=$4',
-        [nome, preco, estoque, codigo], 
-        (error) => {
-            if (error){
-                throw error
+        [nome, preco, estoque, codigo],
+        (error, results) => {
+            if (error || results.rowCount == 0) {
+                return response.status(401).json({ status: 'error', message: 'Não foi possivel atualizar o produto' });
             }
-            response.status(201).json({ status: 'success', message: 'Produto atualizado com sucesso'})
-        }
+            return response.status(201).json({ status: 'success', message: 'Produto atualizado com sucesso' })
+        },
     )
 }
 
-const deleteProduto = (request, response) => {    
+const deleteProduto = (request, response) => {
     const codigo = parseInt(request.params.id)
 
     pool.query(
         'DELETE from produtos where codigo=$1',
-        [codigo], 
-        (error) => {
-            if (error){
-                throw error
+        [codigo],
+        (error, results) => {
+            if (error || results.rowCount == 0) {
+                return response.status(401).json({ status: 'error', message: 'Não foi possivel remover o produto' });
             }
-            response.status(201).json({ status: 'success', message: 'Produto removido com sucesso'})
-        }
+            response.status(201).json({ status: 'success', message: 'Produto removido com sucesso' })
+        },
     )
 }
 
 const getProdutosPorID = (request, response) => {
     const codigo = parseInt(request.params.id)
-    pool.query('SELECT * FROM produtos where codigo = $1', [codigo] ,(error, results) =>{
-        if (error) {
-            throw error
+    pool.query('SELECT * FROM produtos where codigo = $1', [codigo], (error, results) => {
+        if (error || results.rowCount == 0) {
+            return response.status(401).json({ status: 'error', message: 'Não foi possivel recuperar o produto' });
         }
         response.status(200).json(results.rows)
     })
@@ -77,11 +76,11 @@ const getProdutosPorID = (request, response) => {
 
 
 app
-    .route('/produtos')
+    .route('/api/produtos')
     .get(getProdutos)
     .post(addProduto)
     .put(updateProduto)
-app.route('/produtos/:id')
+app.route('/api/produtos/:id')
     .get(getProdutosPorID)
     .delete(deleteProduto)
 
